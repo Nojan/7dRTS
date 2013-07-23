13a9df15bf30f41becfe9da273602c705903b2e0
Joris Vaillant <joris.vaillant@gmail.com>
2013-07-23 19:01:09 +0200

    Add Grid class, a 2D dynamic array.

---
+++ grid.h
@@ -0,0 +1,87 @@
+#ifndef GRID_H
+#define GRID_H
+
+
+// include
+// std
+#include <vector>
+
+
+template <typename T>
+class Grid
+{
+public:
+  Grid();
+  Grid(std::size_t width, std::size_t height);
+
+  std::size_t width() const;
+  std::size_t height() const;
+
+  void resize(std::size_t width, std::size_t height);
+
+  T& operator()(std::size_t x, std::size_t y);
+  const T& operator()(std::size_t x, std::size_t y) const;
+
+private:
+  std::size_t _width, _height;
+  std::vector<T> _data;
+};
+
+
+// definition
+
+
+template <typename T>
+inline Grid<T>::Grid()
+  : _width(0)
+  , _height(0)
+  , _data(0)
+{ }
+
+
+template <typename T>
+inline Grid<T>::Grid(std::size_t width, std::size_t height)
+  : _width(width)
+  , _height(height)
+  , _data(_height*_width)
+{ }
+
+
+template <typename T>
+inline std::size_t Grid<T>::width() const
+{
+  return _width;
+}
+
+
+template <typename T>
+inline std::size_t Grid<T>::height() const
+{
+  return _height;
+}
+
+
+template <typename T>
+inline void Grid<T>::resize(std::size_t width, std::size_t height)
+{
+  _height = height;
+  _width = width;
+  _data.resize(_height*_width);
+}
+
+
+template <typename T>
+inline T& Grid<T>::operator()(std::size_t x, std::size_t y)
+{
+  return _data[x + y*_width];
+}
+
+
+template <typename T>
+inline const T& Grid<T>::operator()(std::size_t x, std::size_t y) const
+{
+  return _data[x + y*_width];
+}
+
+
+#endif // GRID_H
