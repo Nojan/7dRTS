6f3c090524322d0831b7715b586567f829601924
Mathias Labeyrie <mathias.labeyrie@gmail.com>
2013-07-23 19:34:17 +0200

    suppr int2

--- entityposition.h
+++ entityposition.h
@@ -4,34 +4,6 @@
 
 #include "entitymodule.h"
 
-class int2
-{
-public:
-  int2()
-    : _x(0)
-    , _y(0)
-  {
-  }
-
-  int2(int x, int y)
-    : _x(x)
-    , _y(y)
-  {
-  }
-
-  int x() const { return _x; }
-  int y() const { return _y; }
-
-  int2 operator+(const int2& ref)
-  {
-    return int2(x()+ ref.x(), y() + ref.y());
-  }
-
-private:
-  int _x;
-  int _y;
-};
-
 class EntityPosition : public EntityModule
 {
 public:
