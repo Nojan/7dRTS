531f683f4bace1072ebb63df796f2324262ff1d6
Joris Vaillant <joris.vaillant@gmail.com>
2013-07-23 19:04:31 +0200

    Add first map : test

---
+++ hardcodedmap.h
@@ -0,0 +1,18 @@
+#ifndef HARDCODEDMAP_H
+#define HARDCODEDMAP_H
+
+// include
+// core
+#include "gimpimage.h"
+
+namespace core
+{
+
+class HardCodedImage
+{
+  static GimpImage test;
+};
+
+} // core
+
+#endif // HARDCODEDMAP_H
