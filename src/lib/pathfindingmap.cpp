33c475573290f0e9fafd3ae7d4d70832801e136d
Joris Vaillant <joris.vaillant@gmail.com>
2013-07-29 22:47:08 +0200

    Fix a bug in PathFindingMap generation.

--- src/lib/pathfindingmap.cpp
+++ src/lib/pathfindingmap.cpp
@@ -43,29 +43,31 @@ PathFindingMap::PathFindingMap(const GeneralMap* map)
           n.neighbor.push_back(canPos);
         }
       };
-      auto addNeighborDiag = [&](tile_index xoff, tile_index yoff,
-          tile_index w1_xoff, tile_index w1_yoff,
-          tile_index w2_xoff, tile_index w2_yoff)
+      auto addNeighborDiag = [&](tile_index xoff, tile_index yoff)
       {
         TilePos canPos = {x + xoff, y + yoff};
-        TilePos w1Pos = {x + w1_xoff, y + w1_yoff};
-        TilePos w2Pos = {x + w2_xoff, y + w2_yoff};
+        TilePos w1Pos = {x + xoff, y};
+        TilePos w2Pos = {x, y + yoff};
         if(_nodes.inGrid(canPos.x, canPos.y) &&
            (wallSet.find({curPos, w1Pos}) == std::end(wallSet)) &&
            (wallSet.find({w1Pos, curPos}) == std::end(wallSet)) &&
            (wallSet.find({curPos, w2Pos}) == std::end(wallSet)) &&
-           (wallSet.find({w2Pos, curPos}) == std::end(wallSet)))
+           (wallSet.find({w2Pos, curPos}) == std::end(wallSet)) &&
+           (wallSet.find({w1Pos, canPos}) == std::end(wallSet)) &&
+           (wallSet.find({canPos, w1Pos}) == std::end(wallSet)) &&
+           (wallSet.find({w2Pos, canPos}) == std::end(wallSet)) &&
+           (wallSet.find({canPos, w2Pos}) == std::end(wallSet)))
         {
           n.neighbor.push_back(canPos);
         }
       };
 
       addNeighbor(1, 0);
-      addNeighborDiag(1, 1, 1, 0, 0, 1);
-      addNeighborDiag(1, -1, 1, 0, 0, -1);
+      addNeighborDiag(1, 1);
+      addNeighborDiag(1, -1);
       addNeighbor(-1, 0);
-      addNeighborDiag(-1, 1, -1, 0, 0, 1);
-      addNeighborDiag(-1, -1, -1, 0, 0, -1);
+      addNeighborDiag(-1, 1);
+      addNeighborDiag(-1, -1);
       addNeighbor(0, 1);
       addNeighbor(0, -1);
       _nodes(x, y) = n;
