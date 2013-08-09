336b1d2efbca15775ed6e0452b627c2ad76f6b38
Joris Vaillant <joris.vaillant@gmail.com>
2013-08-09 21:52:40 +0200

    register diagonal door edge.

--- src/lib/door/doorposition.cpp
+++ src/lib/door/doorposition.cpp
@@ -14,9 +14,16 @@ DoorPosition::DoorPosition(std::size_t entityId, const Eigen::Vector2f& centerPo
   , _edges(std::move(edges))
 {
   EntityMap& eMap = gameworld().entityMap();
-  for(const EdgePos& e: _edges)
+  // we must fill diagonal case
+  for(const EdgePos& e1: _edges)
   {
-    eMap.registerDoor(e, entityId);
+    for(const EdgePos& e2: _edges)
+    {
+      if((Eigen::Vector2i(e1.from) - Eigen::Vector2i(e2.to)).array().abs().sum() <= 2)
+      {
+        eMap.registerDoor({e1.from, e2.to}, entityId);
+      }
+    }
   }
 }
 
